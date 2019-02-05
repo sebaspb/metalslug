77cb90a55ff98e3647f544d39e0a8cab5c72d7a5
Casa <>
2019-02-05 13:08:14 -0500

    Se restauran los archivos eliminados en el commit anterior

---
+++ Assets/Scripts/proyectiles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proyectiles : MonoBehaviour
+{
+   
+    public static int dañoCohete=2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
